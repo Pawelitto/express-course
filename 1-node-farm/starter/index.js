0ff1ae59bc565064d1705b1eecca35b946c17279
Paweł Pietras <pawel.pietras144@gmail.com>
2024-03-16 17:14:07 +0100

    first commit

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+// Synchroniczna wersja
+// const text = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const output = `Przykładowy tekst: ${text}.\nStworzony w ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', output)
+
+// Asynchroniczna wersja
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log('Błąd odczytu pliku')
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}` , 'utf-8', err => {
+//                 console.log('Zapisano plik')
+//             })
+//         })
+//     })
+// })
+
+// console.log('Czytam plik...')
+
+// Server
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // overview
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+  }
+  // product
+  else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  }
+  // api
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  }
+  // 404
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+    });
+    res.end('<h1>Strona nie znaleziona</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Nasłuchuje na porcie 8000');
+});
